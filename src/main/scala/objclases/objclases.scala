4b32e4fb02d0781c38c52139c713ce67c62552c7
xdgorgola <15-11264@usb.ve>
2022-08-01 11:51:46 -0400

    Simulador implementado

--- src/main/scala/objclases/objclases.scala
+++ src/main/scala/objclases/objclases.scala
@@ -1,5 +1,13 @@
+package objclases
+
 import scala.collection.mutable.LinkedHashMap
 
+/** Clase que representa una clase con vTable
+ *
+ * @param nombre Nombre de la clase
+ * @param padre Padre de la casa. null si no tiene.
+ * @param metodos Lista de metodos definidos en la clase
+ */
 class ObjClases(val nombre: String, val padre: ObjClases, metodos: List[String])
 {
   var vTable = LinkedHashMap[String, String]()
@@ -8,13 +16,7 @@ class ObjClases(val nombre: String, val padre: ObjClases, metodos: List[String])
 
   for m <- metodos do vTable(m) = nombre
 
+  /** Describe la vTable de una clase */
   def describir(): Unit =
     for (n, m) <- vTable do println(s"$n -> $m :: $n")
 }
-
-@main def main() =
-  val claseA = ObjClases("A", null, List("f", "g", "h"))
-  val claseB = ObjClases("B", claseA, List("f", "h", "a"))
-  claseA.describir()
-  println()
-  claseB.describir()
---
+++ src/main/scala/simulador.scala
@@ -0,0 +1,101 @@
+import scala.collection.mutable
+import objclases.ObjClases
+import scala.io.StdIn.readLine
+
+val TOKEN_DESCRIBIR = "DESCRIBIR"
+val TOKEN_CLASS = "CLASS"
+val TOKEN_SALIR = "SALIR"
+
+var clases = mutable.Map[String, ObjClases]()
+
+def simulator_usage() =
+  println("Uso:\n\tCLASS <tipo> [<nombre>]")
+  println("\t\tDefine un nuevo tipo que poseerá métodos con nombres establecidos en la lista proporcionada.")
+
+  println("\n\tDESCRIBIR <NOMBRE>")
+  println("\t\tMuestra la tabla de metodos virtuales del tipo <NOMBRE>")
+
+  println("\n\tSALIR\n\t\tMata\\sale del programa.")
+
+
+def wrong_params() =
+  println("Parametro invalido o numero de parametros incorrecto.")
+  simulator_usage()
+
+
+/** Calcula si hay elementos repetidos en la cola de tokens
+ *
+ * @param tokens Cola de tokens a chequear
+ * @return true si hay tokens repetidos. false en caso contrario
+ */
+def tokens_repetidos(tokens: mutable.Queue[String]): Boolean =
+  for t <- tokens do
+    if tokens.count(x => x == t) > 1 then true
+  false
+
+
+def comando_class(tokens: mutable.Queue[String]): Unit =
+  if tokens.length < 1 then
+    wrong_params()
+    return ()
+
+  val nombre = tokens.dequeue()
+  if clases.contains(nombre) then
+    println(f"El nombre $nombre ya ha sido definido.")
+    return ()
+
+  if tokens.length > 0 then
+    if tokens(0) == ":" then
+      tokens.dequeue()
+      if tokens.length == 0 then
+        wrong_params()
+        return ()
+
+      if tokens_repetidos(tokens) then
+        wrong_params()
+        return ()
+
+      val padre = tokens.dequeue()
+      if !clases.contains(padre) then
+        wrong_params()
+        return ()
+
+      clases(nombre) = ObjClases(nombre, clases(padre), tokens.toList)
+      println(f"Clase $nombre creada exitosamente.")
+      return ()
+
+  if tokens_repetidos(tokens) then
+    wrong_params()
+    return ()
+
+  clases(nombre) = ObjClases(nombre, null, tokens.toList)
+  println(f"Clase $nombre creada exitosamente.")
+
+
+def comando_describir(tokens: mutable.Queue[String]): Unit =
+  if tokens.length != 1 then
+    wrong_params()
+    return ()
+
+  val clase = tokens.dequeue()
+
+  if (!clases.contains(clase)) then
+    println(s"La clase de nombre $clase no ha sido definida.")
+    return ()
+
+  clases(clase).describir()
+
+
+@main def main() =
+  var run = true
+  while run do
+    print("Da tu orden, creador>")
+    var input = readLine()
+    val tokens = mutable.Queue[String](input.split(' '): _*)
+    input = tokens.dequeue().toUpperCase()
+
+    input match
+      case TOKEN_CLASS => comando_class(tokens)
+      case TOKEN_DESCRIBIR => comando_describir(tokens)
+      case TOKEN_SALIR => run = false
+      case _ => wrong_params()
